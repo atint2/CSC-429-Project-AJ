924f64c8546d393cd3824e186410433d23c057d5
slsmitra <slsmitra@users.noreply.github.com>
2025-01-29 19:13:22 -0500

    Create HelloWorld.java

---
+++ Basic Project Repository/model/HelloWorld.java
@@ -0,0 +1,22 @@
+// tabs=4
+//************************************************************
+//	COPYRIGHT 2009/2015 Sandeep Mitra, Michael Steves and T M Rao, The
+//    College at Brockport, State University of New York. -
+//	  ALL RIGHTS RESERVED
+//
+// This file is the product of The College at Brockport and cannot
+// be reproduced, copied, or used in any shape or form without
+// the express written consent of The College at Brockport.
+//************************************************************
+//
+// specify the package
+package model;
+
+public class HelloWorld {
+	
+	public void sayHello()
+	{
+		System.out.println("Hello Collin, Ryan and Steve");
+	}
+}
+
